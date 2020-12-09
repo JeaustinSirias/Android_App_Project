8394950632c86347a9f9eeaaa24fb9f9e4ef4cfc
Jfecoren <jfecoren@gmail.com>
2020-12-09 15:37:26 -0600

    bluetooth adapter state

--- app/src/main/java/com/example/bluetoothrgbledcontroller/MainActivity.java
+++ app/src/main/java/com/example/bluetoothrgbledcontroller/MainActivity.java
@@ -1,26 +1,46 @@
 package com.example.bluetoothrgbledcontroller;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private TextView bar_progress;
     private SeekBar intensity_bar;
+    Button power_button;
+
+    // bluetooth variables/elements
+    Button bluetooth_button;
+    public BluetoothAdapter adapter_bluetooth;
+    private boolean connected_to;
+    Intent bluetooth_enablingIntent;
+    int requestCode_bluetooth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // conectivity button
-        Button bluetooth_button = findViewById(R.id.connect_button);
+        // conectivity to bluetooth
+        bluetooth_button = findViewById(R.id.connect_button);
+        adapter_bluetooth = BluetoothAdapter.getDefaultAdapter();
+        connected_to  = false;
+        bluetooth_enablingIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        requestCode_bluetooth = 1;
+
+        BluetoothConnection();
 
         // turn on/off button
-        Button power_button = findViewById(R.id.led_button);
+        power_button = findViewById(R.id.led_button);
 
         // color light elements
         Button white_button = findViewById(R.id.white_button);
@@ -36,10 +56,11 @@ public class MainActivity extends AppCompatActivity {
         //  intensity LED progress bar control
         intensity_bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             int intensity = 0;
+            @SuppressLint("DefaultLocale")
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 intensity = progress;
-                bar_progress.setText(""+intensity);
+                bar_progress.setText(String.format("%d", intensity));
             }
 
             @Override
@@ -50,15 +71,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
                 bar_progress.setVisibility(View.INVISIBLE);
-
-            }
-        });
-
-        // Bluetooth click on listener
-        bluetooth_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
             }
         });
 
@@ -109,6 +121,49 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @SuppressLint("MissingSuperCall")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == requestCode_bluetooth){
+            if(resultCode == RESULT_OK){
+                Toast.makeText(getApplicationContext(), "Bluetooth has been enabled", Toast.LENGTH_LONG).show();
+                connected_to = true;
+            }else if(resultCode == RESULT_CANCELED){
+                Toast.makeText(getApplicationContext(), "Bluetooth cannot be enabled", Toast.LENGTH_LONG).show();
+
+            }
+        }
+    }
+
+    private void BluetoothConnection() {
+        // Bluetooth click on listener
+        bluetooth_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // if bluetooth is not connected
+                if(!connected_to){
+                    if(adapter_bluetooth == null){
+                        Toast.makeText(getApplicationContext(), "Bluetooth not supported on this device", Toast.LENGTH_LONG).show();
+                    }else{
+                        if(!adapter_bluetooth.isEnabled()){
+                            startActivityForResult(bluetooth_enablingIntent, requestCode_bluetooth);
+                        }
+                    }
+                }
+
+                // if bluetooth is connected to a device
+                if(connected_to){
+                    if(adapter_bluetooth.isEnabled()){
+                        adapter_bluetooth.disable();
+                        connected_to = false;
+                    }
+                }
+
+            }
+        });
+
+
+    }
 
 
 }
