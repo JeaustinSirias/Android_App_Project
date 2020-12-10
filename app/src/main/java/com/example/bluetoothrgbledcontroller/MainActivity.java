8f6dae5eda363f73439be16dd6834317d9c4985f
Jfecoren <jfecoren@gmail.com>
2020-12-09 21:19:52 -0600

    connection stablished (list activity deleted)

--- app/src/main/java/com/example/bluetoothrgbledcontroller/MainActivity.java
+++ app/src/main/java/com/example/bluetoothrgbledcontroller/MainActivity.java
@@ -6,17 +6,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.Set;
+import java.io.IOException;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     private TextView bar_progress;
@@ -27,9 +27,11 @@ public class MainActivity extends AppCompatActivity {
     Button bluetooth_button;
     public BluetoothAdapter adapter_bluetooth;
     private boolean connected_to;
+    private boolean noDisconnect = true;
     Intent bluetooth_enablingIntent;
     int requestCode_bluetooth;
-    ListView showDevices;
+    //static final UUID mUUID = UUID.fromString("00001133-0000-1000-8000-00805f9b34fb");
+    private final UUID mUUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -126,17 +128,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
+    // message to print result of connection trying
     @SuppressLint("MissingSuperCall")
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if(requestCode == requestCode_bluetooth){
             if(resultCode == RESULT_OK){
                 Toast.makeText(getApplicationContext(), "Bluetooth has been enabled", Toast.LENGTH_LONG).show();
-                connected_to = true;
             }else if(resultCode == RESULT_CANCELED){
                 Toast.makeText(getApplicationContext(), "Bluetooth cannot be enabled", Toast.LENGTH_LONG).show();
-
             }
         }
     }
@@ -147,7 +147,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                // if bluetooth is not connected
+                // if bluetooth is not enabled*********
                 if(!connected_to){
                     if(adapter_bluetooth == null){
                         Toast.makeText(getApplicationContext(), "Bluetooth not supported on this device", Toast.LENGTH_LONG).show();
@@ -156,20 +156,61 @@ public class MainActivity extends AppCompatActivity {
                             startActivityForResult(bluetooth_enablingIntent, requestCode_bluetooth);
                         }
                     }
-                }
+                }//************************************
 
                 // if bluetooth is connected to a device
-                //if(connected_to)
-                if(adapter_bluetooth.isEnabled()){
-                    openListDevicesActivity();
+
+                BluetoothSocket hc_socket = null;
 
 
+                if(adapter_bluetooth.isEnabled() && !connected_to){
+                    //BluetoothDevice hc_05 = adapter_bluetooth.getRemoteDevice("54:8D:5A:20:7B:EA");//"98:D3:61:F5:CB:58"); PC
+                    BluetoothDevice hc_05 = adapter_bluetooth.getRemoteDevice("98:D3:61:F5:CB:58");//HC-05
+                    int tryToConnect = 0;
+                    do {
+                        try {
+                            hc_socket = hc_05.createRfcommSocketToServiceRecord(mUUID);
+                            hc_socket.connect();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
 
+                        if(tryToConnect > 3){
+                            tryToConnect = 0;
+                            Toast.makeText(getApplicationContext(), "Connection failed", Toast.LENGTH_LONG).show();
+                            break;
+                        }else{
+                            tryToConnect++;
+                        }
+
+                    }while(!hc_socket.isConnected());
+
+                    if(hc_socket.isConnected()){
+                        connected_to = true;
+                        Toast.makeText(getApplicationContext(), "Connection Successful", Toast.LENGTH_LONG).show();
+                        bluetooth_button.setText(R.string.on_connect);
+                    }
+
+                    //if(connected_to)
                     /*if(adapter_bluetooth.isEnabled()){
                         adapter_bluetooth.disable();
                         connected_to = false;
                     }*/
+                }else if(!connected_to){
+                    Toast.makeText(getApplicationContext(), "Bluetooth not enabled", Toast.LENGTH_LONG).show();
                 }
+
+                /*if(hc_socket.isConnected()) {
+                    try {
+                        hc_socket.close();
+                        Toast.makeText(getApplicationContext(), "Connection closed", Toast.LENGTH_LONG).show();
+                        connected_to = false;
+                        bluetooth_button.setText(R.string.connect);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }*/
+
 
             }
         });
@@ -177,11 +218,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void openListDevicesActivity() {
-        Intent openList = new Intent(this, ListDevicesActivity.class);
-        startActivity(openList);
-
-    }
 
 
 }
